2f2fd2a6fe55bcde6ac80ae702f07ca0f9322f44
previzdaniel <dani.2000.2x@gmail.com>
2020-09-16 08:35:14 +0200

    Tabla osztaly letrehozasa

--- kiralyno/Program.cs
+++ kiralyno/Program.cs
@@ -8,6 +8,47 @@ namespace kiralyno
 {
     class Program
     {
+
+        class Tabla
+        {
+            char[,] matrix = new char[8, 8];
+            private char uresCella;
+            private int uresOszlopokSzama;
+            private int uresSorokSzama;
+
+
+            public Tabla(char ch)
+            {
+
+            }
+            public void Elhelyez()
+            {
+
+            }
+
+            public void FajlbaIr()
+            {
+
+            }
+
+            public void Megjelenit()
+            {
+
+            }
+
+
+
+            public int UresOszlop()
+            {
+
+            }
+
+            public int UresSor()
+            {
+
+            }
+
+        }
         static void Main(string[] args)
         {
 
